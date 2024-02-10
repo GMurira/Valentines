97fbcda788d82f0c17068e8a201346819965d29a
GMurira <nguyaimurira@gmail.com>
2024-02-10 18:53:13 +0300

    Design Credits

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Valentines
+# Valentines 
+# Design inspired by Brychan Artanto https://dribbble.com/shots/15348811-Valentine-App
+#
+#
--- app/src/main/java/com/example/valentineitemsapp/ui/theme/screens/ValentineHomeScreen.kt
+++ app/src/main/java/com/example/valentineitemsapp/ui/theme/screens/ValentineHomeScreen.kt
@@ -1,2 +1,24 @@
 package com.example.valentineitemsapp.ui.theme.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Valentine Home Screen
+ */
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier){
+    Column {
+
+    }
+}
+/**
+ * Velentine preview
+ */
+@Preview
+@Composable
+fun HomeScreenPreview(){
+
+}
